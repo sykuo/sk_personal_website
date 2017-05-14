144d7d7dfae5d2712047823026144958d0ab9455
Steven Kuo <sykuo@uci.edu>
2017-05-13 20:53:27 -0700

    Fixed google maps to display correct location + adding header
    Google maps marker and center of map changed to Irvine,CA. Also changed API key to personal one.

--- stevenkuo_1weekchallenge/js/grayscale.js
+++ stevenkuo_1weekchallenge/js/grayscale.js
@@ -48,7 +48,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(33.6846, -117.8265), // Irvine
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(33.6846, -117.8265);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
